a213e4972a63a1f5c04f112d15c0e7f850d2d86c
junepro <57428866+junepro@users.noreply.github.com>
2022-03-14 11:12:29 +0900

    프로젝트 설정 및 계정 도메인

---
+++ src/main/java/com/reolle/ReolleApplication.java
@@ -0,0 +1,13 @@
+package com.reolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReolleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReolleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reolle/ReolleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReolleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
