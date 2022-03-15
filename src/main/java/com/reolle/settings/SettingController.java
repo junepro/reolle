8e47c8e88c0171e87aacc073586b0b4578debdd7
junepro <57428866+junepro@users.noreply.github.com>
2022-03-15 13:57:53 +0900

    패스워드 수정

--- src/main/java/com/reolle/settings/SettingController.java
+++ src/main/java/com/reolle/settings/SettingController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -17,8 +19,16 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class SettingController {
 
+    @InitBinder("passwordForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(new PasswordFormValidator());
+    }
+
     public static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
     public static final String SETTINGS_PROFILE_URL = "/settings/profile";
+
+    public static final String SETTINGS_PASSWORD_VIEW_NAME = "settings/password";
+    public static final String SETTINGS_PASSWORD_URL = "/settings/password";
 
     private final AccountService accountService;
 
@@ -34,7 +44,7 @@ public class SettingController {
     @PostMapping(SETTINGS_PROFILE_URL)  //여기 프로필에 일단 빈생성자가 있으면 nullpoint 오류 발생 so Profile에 noargsconstructor 설정
     public String updateProfile(@CurrentAccount Account account, @Valid Profile profile, Errors errors,
                                 Model model, RedirectAttributes attributes) {
-                                            //redirect 한번만 보낼 메시지 일때 사용
+        //redirect 한번만 보낼 메시지 일때 사용
         if (errors.hasErrors()) {
             model.addAttribute(account);
             return SETTINGS_PROFILE_VIEW_NAME;
@@ -42,6 +52,27 @@ public class SettingController {
         accountService.updateProfile(account, profile);
         attributes.addFlashAttribute("message", "프로필을 수정했습니다.");
         return "redirect:" + SETTINGS_PROFILE_URL;
+    }
+
+    @GetMapping(SETTINGS_PASSWORD_URL)
+    public String passwordUpdateForm(@CurrentAccount Account account, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(new PasswordForm());
+        return SETTINGS_PASSWORD_VIEW_NAME;
+
+    }
+
+    @PostMapping(SETTINGS_PASSWORD_URL)
+    public String updatePassword(@CurrentAccount Account account, @Valid PasswordForm passwordForm,
+                                 Errors errors, Model model, RedirectAttributes attributes) {
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            return SETTINGS_PASSWORD_VIEW_NAME;
+        }
+
+        accountService.updatePassword(account, passwordForm.getNewPassword());
+        attributes.addFlashAttribute("message", "패스워드를 변경했습니다");
+        return "redirect:" + SETTINGS_PASSWORD_URL;
     }
 }
 
