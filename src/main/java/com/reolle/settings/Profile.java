c874f317ffad3d693c514fee73e9bf65a5f45d1f
junepro <57428866+junepro@users.noreply.github.com>
2022-03-15 12:33:13 +0900

    프로필 수정 테스트

--- src/main/java/com/reolle/settings/Profile.java
+++ src/main/java/com/reolle/settings/Profile.java
@@ -3,17 +3,22 @@ package com.reolle.settings;
 import com.reolle.domain.Account;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor //빈생성자가 있을경우 널포인트 인셉션 발생 가능성
 public class Profile {
 
+    @Length(max = 35)
     private String bio;
 
+    @Length(max = 50)
     private String url;
 
+    @Length(max = 50)
     private String occupation;
 
+    @Length(max = 50)
     private String location;
 
     public Profile(Account account) {
---
+++ src/test/java/com/reolle/settings/SettingsControllerTest.java
@@ -0,0 +1,91 @@
+package com.reolle.settings;
+
+
+import com.reolle.WithAccount;
+import com.reolle.account.AccountRepository;
+import com.reolle.account.AccountService;
+import com.reolle.account.SignUpForm;
+import com.reolle.domain.Account;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SettingsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @AfterEach
+    void afterEach() {
+        accountRepository.deleteAll(); //밑에 수정할때마다 june 관련 계정 계속 생성하기떄문에
+    }
+
+    @WithAccount("june")
+    @DisplayName("프로필 수정폼")
+    @Test
+    void updateProfileForm() throws Exception {
+        String bio = "짧은 소개를 수정하는 경우 ";
+        mockMvc.perform(get(SettingController.SETTINGS_PROFILE_URL))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"));
+    }
+
+    @WithAccount("june")
+    @DisplayName("프로필 수정하기 - 입력값 정상")
+    @Test
+    void updateProfile() throws Exception {
+        String bio = "짧은 소개를 수정하는 경우 ";
+        mockMvc.perform(post(SettingController.SETTINGS_PROFILE_URL)
+                        .param("bio", bio)
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(SettingController.SETTINGS_PROFILE_URL))
+                .andExpect(flash().attributeExists("message"));
+
+        Account june = accountRepository.findByNickname("june");
+        assertEquals(bio, june.getBio());
+    }
+
+
+    @WithAccount("june")
+    @DisplayName("프로필 수정하기 - 입력값 에러")
+    @Test
+    void updateProfile_error() throws Exception {
+        String bio = "길게 수정해야 하는 경우~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ";
+        mockMvc.perform(post(SettingController.SETTINGS_PROFILE_URL)
+                        .param("bio", bio)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingController.SETTINGS_PROFILE_VIEW_NAME))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"))
+                .andExpect(model().hasErrors());
+
+        Account june = accountRepository.findByNickname("june");
+        assertNull(june.getBio());
+    }
+}
