199f6a15c4f26a07cf220d21e02af37fb325b4f9
junepro <57428866+junepro@users.noreply.github.com>
2022-03-20 15:10:41 +0900

    모임 만들기

---
+++ src/main/java/com/reolle/event/EventController.java
@@ -0,0 +1,56 @@
+package com.reolle.event;
+
+import com.reolle.account.CurrentAccount;
+import com.reolle.domain.Account;
+import com.reolle.domain.Event;
+import com.reolle.domain.Study;
+import com.reolle.event.form.EventForm;
+import com.reolle.study.StudyService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/study/{path}")
+public class EventController {
+
+    private final StudyService studyService;
+    private final EventService eventService;
+    private final ModelMapper modelMapper;
+    private final EventValidator eventValidator;
+
+    @InitBinder("eventForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(eventValidator);
+    }
+
+    @GetMapping("/new-event")
+    public String newEventForm(@CurrentAccount Account account, @PathVariable String path, Model model) {
+        Study study = studyService.getStudyToUpdateStatus(account, path);
+        model.addAttribute(study);
+        model.addAttribute(account);
+        model.addAttribute(new EventForm());
+        return "event/form";
+    }
+
+    @PostMapping("/new-event")
+    public String newEventSubmit(@CurrentAccount Account account, @PathVariable String path,
+                                 @Valid EventForm eventForm, Errors errors, Model model) {
+        Study study = studyService.getStudyToUpdateStatus(account, path);
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            model.addAttribute(study);
+            return "event/form";
+        }
+
+        Event event = eventService.createEvent(modelMapper.map(eventForm, Event.class), study, account);
+        return "redirect:/study/" + study.getEncodedPath() + "/events/" + event.getId();
+    }
+}
---
+++ src/main/java/com/reolle/event/EventService.java
@@ -0,0 +1,25 @@
+package com.reolle.event;
+
+import com.reolle.domain.Account;
+import com.reolle.domain.Event;
+import com.reolle.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public Event createEvent(Event event, Study study, Account account) {
+        event.setCreateBy(account);
+        event.setCreatedDateTime(LocalDateTime.now());
+        event.setStudy(study);
+        return eventRepository.save(event);
+    }
+}
