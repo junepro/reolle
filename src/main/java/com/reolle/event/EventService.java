0b61e7d85aa003a76956866e67030414eeef44fc
junepro <57428866+junepro@users.noreply.github.com>
2022-03-21 14:19:56 +0900

    모임 삭제기능

--- src/main/java/com/reolle/event/EventService.java
+++ src/main/java/com/reolle/event/EventService.java
@@ -29,4 +29,9 @@ public class EventService {
     public void updateEvent(Event event, EventForm eventForm) {
         modelMapper.map(eventForm, event);
     }
+
+    public void deleteEvent(Event event) {
+        eventRepository.delete(event);
+
+    }
 }
