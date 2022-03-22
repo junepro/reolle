01ac75555dfa2e83b98ed8d933b4331566c98b4a
junepro <57428866+junepro@users.noreply.github.com>
2022-03-22 15:55:49 +0900

    paging 적용

--- src/main/java/com/reolle/modules/main/MainController.java
+++ src/main/java/com/reolle/modules/main/MainController.java
@@ -6,6 +6,10 @@ import com.reolle.modules.study.Study;
 import com.reolle.modules.study.StudyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.Banner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,9 +36,12 @@ public class MainController {
     }
 
     @GetMapping("/search/study")
-    public String searchStudy(String keyword, Model model) {
-        List<Study> studyList = studyRepository.findByKeyword(keyword);
-        model.addAttribute(studyList);
+    public String searchStudy(String keyword, Model model,
+                              @PageableDefault(size = 9, sort = "publishedDateTime", direction = Sort.Direction.DESC)
+                                      Pageable pageable) {
+
+        Page<Study> studyPage = studyRepository.findByKeyword(keyword,pageable);
+        model.addAttribute("studyPage", studyPage);
         model.addAttribute("keyword", keyword);
         return "search";
     }
--- src/main/java/com/reolle/modules/study/StudyRepositoryExtensionImpl.java
+++ src/main/java/com/reolle/modules/study/StudyRepositoryExtensionImpl.java
@@ -1,10 +1,14 @@
 package com.reolle.modules.study;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.reolle.modules.account.QAccount;
 import com.reolle.modules.tag.QTag;
 import com.reolle.modules.zone.QZone;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
@@ -17,7 +21,7 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
     }
 
     @Override
-    public List<Study> findByKeyword(String keyword) {
+    public Page<Study> findByKeyword(String keyword, Pageable pageable) {
         QStudy study = QStudy.study;
         JPQLQuery<Study> query = from(study).where(study.published.isTrue()
                         .and(study.title.containsIgnoreCase(keyword)) //seq문자열중 search관련 문자열 포함되면 true 리턴
@@ -29,6 +33,9 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
                 .leftJoin(study.members, QAccount.account).fetchJoin() //join한 데이터만가져오는것
                 .distinct();
 
-        return query.fetch();
+        JPQLQuery<Study> pageableQuery = getQuerydsl().applyPagination(pageable, query);
+        QueryResults<Study> fetchResults = pageableQuery.fetchResults();
+
+        return new PageImpl<>(fetchResults.getResults(), pageable, fetchResults.getTotal());
     }
 }
